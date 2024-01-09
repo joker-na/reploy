442d14ea3dcb7be1440f72a1717b30d6d31525b2
joker-na <155509416+joker-na@users.noreply.github.com>
2024-01-09 23:29:17 +0800

    Create dd.sh

---
+++ dd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo -i
+wget --no-check-certificate -qO InstallNET.sh "https://raw.githubusercontent.com/leitbogioro/Tools/master/Linux_reinstall/InstallNET.sh"
+chmod a+x InstallNET.sh
+bash InstallNET.sh -debian 12 -pwd hp6#dT0#s4t5t
+sleep 10
+reboot
