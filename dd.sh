b57b6da3a6f0fc4b26d2bbb406d10d632363536b
joker-na <155509416+joker-na@users.noreply.github.com>
2024-01-10 00:14:27 +0800

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -5,4 +5,4 @@ wget --no-check-certificate -qO InstallNET.sh "https://raw.githubusercontent.com
 chmod a+x InstallNET.sh
 bash InstallNET.sh -debian 12 -pwd hp6#dT0#s4t5t
 sleep 10
-(reboot && exit 0) &
+reboot
