7a8bdc122b879f012f3bc3d75496b1ee3656737a
joker-na <155509416+joker-na@users.noreply.github.com>
2024-05-27 19:51:38 +0800

    Update caddy.sh

--- caddy.sh
+++ caddy.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get install -y debian-keyring debian-archive-keyring apt-transport-https
 
 # 导入Caddy的公钥
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo apt-key add -
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 
 # 导入Caddy的APT repo
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/deb/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
