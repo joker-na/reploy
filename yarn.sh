5b491c124b4df96849b460c711f0ef24f7e24ee7
joker-na <155509416+joker-na@users.noreply.github.com>
2024-05-27 14:53:57 +0800

    Create yarn.sh

---
+++ yarn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 更新系统的软件包列表
+sudo apt update
+
+# 安装 Node.js 和 npm
+sudo apt install -y nodejs npm
+
+# 使用 npm 安装 yarn
+sudo npm install -g yarn
+
+# 使用 yarn 安装 pm2
+sudo yarn global add pm2
